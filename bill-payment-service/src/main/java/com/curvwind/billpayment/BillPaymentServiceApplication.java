44c5f3740732144c282ab8fb3e21d92ad67843eb
emmanuel <emmantoksjr@gmail.com>
2022-01-02 22:51:58 +0100

    WIP: created necessary directories

---
+++ bill-payment-service/src/main/java/com/curvwind/billpayment/BillPaymentServiceApplication.java
@@ -0,0 +1,11 @@
+package com.curvwind.billpayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillPaymentServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BillPaymentServiceApplication.class, args);
+    }
+}
